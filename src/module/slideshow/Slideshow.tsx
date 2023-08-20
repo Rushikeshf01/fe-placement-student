6dce29c5d1394bbd642be6b238469cc27a62e1a6
Rushikeshf01 <falakrushikesh83@gmail.com>
2023-08-20 23:00:19 +0530

    slide images updated

---
+++ src/module/slideshow/Slideshow.tsx
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+import Image from "next/image";
+
+const slideshowImages:string[] = ["sliderImages/1.webp","sliderImages/2.webp", "sliderImages/3.webp", "sliderImages/4.webp","sliderImages/5.webp"];
+
+function Slideshow() {
+    const [images, setImages] = useState(slideshowImages[0])
+  return (
+    
+    
+    <div className="w-2/3">
+    <div className="w-full h-full bg-gray-200">
+        <Image
+          src="/Slider-Images/1.webp"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-full h-full"
+        />
+    </div>
+  </div>
+
+  )
+}
+
+export default Slideshow
