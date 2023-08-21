fdd77f9b5028709bcf9cd849f0c95924c90dd220
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-08-21 14:22:45 +0530

    login added

--- src/module/login/Login.tsx
+++ src/module/login/Login.tsx
@@ -1,4 +1,5 @@
 import { ApplicationConstant } from "@/constant/applicationConstant";
+import { Button, TextField } from "@mui/material";
 import Link from "next/link";
 import React, { useState } from "react";
 
@@ -17,32 +18,43 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <label htmlFor="email">Email: </label>
-      <input
-        type="email"
-        id="email"
-        value={student.email}
-        onChange={(e) => {
-          setStudent({ ...student, email: e.target.value });
-        }}
-      />
-      <br />
-      <label htmlFor="password">Password: </label>
-      <input
-        type="password"
-        id="password"
-        value={student.password}
-        onChange={(e) => {
-          setStudent({ ...student, password: e.target.value });
-        }}
-      />
-      <br />
-      <button onClick={onLogin}>Login</button>
-      <br />
-      <Link href={ApplicationConstant.REGISTER_URL_PATH}>Register here</Link>
-    </div>
+    <>
+      <p className="mt-6 text-3xl font-bold">Login</p>
+      <div className="grid grid-cols-1 gap-4 mt-3">
+        <TextField
+          label="Email"
+          type="email"
+          value={student.email}
+          required
+          variant="standard"
+          onChange={(e) => {
+            setStudent({ ...student, email: e.target.value });
+          }}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={student.password}
+          required
+          variant="standard"
+          onChange={(e) => {
+            setStudent({ ...student, password: e.target.value });
+          }}
+        />
+      </div>
+      <Button variant="outlined" className="mt-4" onClick={onLogin}>
+        Log in
+      </Button>
+      <div className="mt-3.5">
+        {`Don't have an student account?`}{" "}
+        <Link
+          href={ApplicationConstant.REGISTER_URL_PATH}
+          className="font-semibold"
+        >
+          Register here
+        </Link>
+      </div>
+    </>
   );
 };
 
