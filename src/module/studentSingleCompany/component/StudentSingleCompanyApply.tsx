77fcf04aa9156e8bbc377c0d8c7ea7d7a7f59278
Rushikeshf01 <falakrushikesh83@gmail.com>
2023-09-06 11:20:18 +0530

    apply route created

--- src/module/studentSingleCompany/component/StudentSingleCompanyApply.tsx
+++ src/module/studentSingleCompany/component/StudentSingleCompanyApply.tsx
@@ -1,5 +1,7 @@
+import { ApplicationConstant } from "@/constant/applicationConstant";
 import { SingleCompanyItemType } from "@/utils/types";
 import { Divider } from "@mui/material";
+import { useRouter, usePathname } from "next/navigation";
 import React from "react";
 
 const StudentSingleCompanyApply = ({
@@ -7,6 +9,12 @@ const StudentSingleCompanyApply = ({
 }: {
   singleCompanyItem: SingleCompanyItemType;
 }) => {
+  const router = useRouter();
+  const path = usePathname();
+  const handleApply = () => {
+    console.log(`${path}/apply`);
+    router.push(`${path}/apply`);
+  }
   return (
     <>
       <Divider className="my-2" />
@@ -16,6 +24,7 @@ const StudentSingleCompanyApply = ({
             ? "cursor-not-allowed"
             : "hover:bg-cyan-500"
         } p-2 px-8 rounded-full text-white text-md font-semibold bg-cyan-400`}
+        onClick = {handleApply}
       >
         Apply
       </button>
